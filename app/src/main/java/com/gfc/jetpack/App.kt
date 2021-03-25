d76d185bb287fcb6530e19446e31f2800870bde3
guofucg <773690167@qq.com>
2021-03-25 16:49:08 +0800

    first

---
+++ app/src/main/java/com/gfc/jetpack/App.kt
@@ -0,0 +1,21 @@
+package com.gfc.jetpack
+
+import android.app.Application
+import com.gfc.jetpack.base.BaseApplication
+
+/**
+ *
+ * @Description: java类作用描述
+ * @Author: Mark Guo
+ * @CreateDate: 2021/3/25 16:39
+ * @UpdateUser:
+ * @UpdateDate: 2021/3/25 16:39
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+class App : BaseApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/gfc/jetpack/base/BaseApplication.kt
@@ -0,0 +1,28 @@
+package com.gfc.jetpack.base
+
+import android.app.Application
+import android.content.Context
+
+/**
+ *
+ * @Description: java类作用描述
+ * @Author: Mark Guo
+ * @CreateDate: 2021/3/25 16:40
+ * @UpdateUser:
+ * @UpdateDate: 2021/3/25 16:40
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+open class BaseApplication :Application() {
+
+    companion object{
+        private lateinit var baseApplication: BaseApplication
+
+        fun getContext():Context{
+
+            return baseApplication
+        }
+
+    }
+
+}
